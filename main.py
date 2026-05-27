bc6630084d6cfe1857109065965923fa0f2d57f7
hannjimnz <hannjimnz@gmail.com>
2026-05-26 20:24:52 -0600

    Implemented SQL and imports

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import sqlite3
+from typing import Optional, List 
+
+app = FastAPI(title= "To do list")
+
+#aqui configuraremos los CORS, esto es para que no haya problema con el frontend al momento de conectarlo 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True ,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#creacion de los modelos de datos, esto es para validar lo que entra y sale del api
+
+class TaskCreate(BaseModel):
+    title: str
+
+class Task(BaseModel):
+    id: int
+    title: str
+    completed:bool
+
+#aqui vamos a inicializar la base de datos sqlite cada vez que se arranque 
+
+def init_db():
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            completed BOOLEAN NOT NULL DEFAULT 0
+        )
+    """)
+    conn.commit()
+    conn.close()
+init_db()
